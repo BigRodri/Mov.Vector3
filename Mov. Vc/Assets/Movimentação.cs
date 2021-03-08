eeeae3ada4f4b87238559925b3a62ef27c868a92
BigRodri <rodrigonnes@icloud.com>
2021-03-08 14:41:42 -0300

    Trabalho Inteligencia Artificial

---
+++ Mov. Vc/Assets/Movimentação.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação : MonoBehaviour
+{
+    public Transform GO1;
+    public float V1 = 5f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (GO1.position - transform.position).normalized*V1*Time.deltaTime;
+    }
+}
---
+++ Mov. Vc/Assets/Movimentação2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação2 : MonoBehaviour
+{
+    public List<Transform> place = new List<Transform>();
+    Transform finalplace;
+    int position = 0;
+    bool voltar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalplace = place[position];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, finalplace.position) > 0.2f)
+            transform.position += (finalplace.position - transform.position).normalized * 8f * Time.deltaTime;
+        else
+            ChangePatrolPoint();
+    }
+    void ChangePatrolPoint()
+    {
+        if (!voltar)
+            position++;
+        else
+            position--;
+
+        if (position >= place.Count)
+        {
+            position = (place.Count - 1);
+            voltar = true;
+        }
+        else if (position < 0)
+        {
+            position = 0;
+            voltar = false;
+        }
+
+        finalplace = place[position];
+    }
+}
